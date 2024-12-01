6296fe6f11f796a9cae0ca96a84b37e05557414f
Reinis Danne <rei4dan@gmail.com>
2024-12-01 23:12:40 +0200

    Ectract day01 module

---
+++ src/day01.rs
@@ -0,0 +1,84 @@
+use super::Args;
+use std::iter::zip;
+
+pub(crate) fn run(args: Args) -> usize {
+    let filename = args.filename();
+    match args.part {
+        1 => part1(filename),
+        2 => part2(filename),
+        _ => todo!(),
+    }
+}
+
+fn extract(filename: String) -> (Vec<i64>, Vec<i64>) {
+    let contents =
+        std::fs::read_to_string(dbg!(filename)).expect("should have been able to read the file");
+    // println!("{contents}");
+    let pairs: Vec<Vec<&str>> = contents
+        .trim()
+        .lines()
+        .map(|x| x.split_whitespace().collect::<Vec<&str>>())
+        .collect();
+    // dbg!(&pairs);
+    let mut first: Vec<i64> = Vec::new();
+    let mut second: Vec<i64> = Vec::new();
+    for pair in pairs {
+        first.push(pair[0].parse().unwrap());
+        second.push(pair[1].parse().unwrap());
+    }
+    // dbg!(&first);
+    // dbg!(&second);
+    (first, second)
+}
+
+fn part1(filename: String) -> usize {
+    let (mut first, mut second) = extract(filename);
+    first.sort();
+    second.sort();
+    // dbg!(&first);
+    // dbg!(&second);
+    let terms: Vec<i64> = zip(first, second).map(|(a, b)| (a - b).abs()).collect();
+    // dbg!(&terms);
+    let result: i64 = terms.iter().sum();
+    dbg!(result);
+    result as usize
+}
+
+fn part2(filename: String) -> usize {
+    let (first, second) = extract(filename);
+    let terms: Vec<usize> = first
+        .iter()
+        .map(|x| *x as usize * second.iter().filter(|y| *y == x).count())
+        .collect();
+    // dbg!(&terms);
+    let result: usize = terms.iter().sum();
+    dbg!(result);
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn p1e1() {
+        let args = Args {
+            day: Some(1),
+            part: 1,
+            example: Some(1),
+        };
+        let result = run(args);
+        assert_eq!(result, 11);
+    }
+
+    #[test]
+    fn p2e1() {
+        let args = Args {
+            day: Some(1),
+            part: 2,
+            example: Some(1),
+        };
+        let result = run(args);
+        assert_eq!(result, 31);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::iter::zip;
-
 use clap::Parser;
+
+mod day01;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about=None)]
@@ -9,8 +9,29 @@ struct Args {
     day: Option<u8>,
     #[arg(short, long)]
     example: Option<u8>,
-    #[arg(short, long)]
-    part: Option<u8>,
+    #[arg(short, long, default_value_t = 1)]
+    part: u8,
+}
+
+impl Args {
+    fn example_filename(self: &Args) -> String {
+        let Some(day) = self.day else { todo!() };
+        let Some(number) = self.example else { todo!() };
+        format!("data/{day:02}/example{number}")
+    }
+
+    fn input_filename(self: &Args) -> String {
+        let Some(day) = self.day else { todo!() };
+        format!("data/{day:02}/input")
+    }
+
+    fn filename(self: &Args) -> String {
+        if self.example.is_some() {
+            self.example_filename()
+        } else {
+            self.input_filename()
+        }
+    }
 }
 
 fn main() {
@@ -18,93 +39,7 @@ fn main() {
     let Some(day) = args.day else { todo!() };
 
     match day {
-        1 => run(day, args),
+        1 => day01::run(args),
         _ => todo!(),
-    }
-}
-
-fn run(day: u8, args: Args) {
-    match day {
-        1 => day01(args),
-        _ => todo!(),
-    }
-}
-
-fn day01(args: Args) {
-    if let Some(number) = args.example {
-        if let Some(part) = args.part {
-            example(1, part, number);
-        } else {
-            example(1, 1, number);
-            example(1, 2, number);
-        }
-    } else if let Some(part) = args.part {
-        input(1, part)
-    } else {
-        input(1, 1);
-        input(1, 2);
-    }
-}
-
-fn example(day: u8, part: u8, number: u8) {
-    let filename = format!("data/{day:02}/example{number}");
-    if part == 1 {
-        part1(filename);
-    } else {
-        part2(filename);
-    }
-}
-
-fn input(day: u8, part: u8) {
-    let filename = format!("data/{day:02}/input");
-    if part == 1 {
-        part1(filename);
-    } else {
-        part2(filename);
-    }
-}
-
-fn extract(filename: String) -> (Vec<i64>, Vec<i64>) {
-    let contents =
-        std::fs::read_to_string(dbg!(filename)).expect("should have been able to read the file");
-    // println!("{contents}");
-    let lines: Vec<&str> = contents.trim().split("\n").collect();
-    // dbg!(&lines);
-    let pairs: Vec<Vec<&str>> = lines
-        .iter()
-        .map(|x| x.split_whitespace().collect::<Vec<&str>>())
-        .collect();
-    // dbg!(&pairs);
-    let mut first: Vec<i64> = Vec::new();
-    let mut second: Vec<i64> = Vec::new();
-    for pair in pairs {
-        first.push(pair[0].parse().unwrap());
-        second.push(pair[1].parse().unwrap());
-    }
-    // dbg!(&first);
-    // dbg!(&second);
-    (first, second)
-}
-
-fn part1(filename: String) {
-    let (mut first, mut second) = extract(filename);
-    first.sort();
-    second.sort();
-    // dbg!(&first);
-    // dbg!(&second);
-    let terms: Vec<i64> = zip(first, second).map(|(a, b)| (a - b).abs()).collect();
-    // dbg!(&terms);
-    let result: i64 = terms.iter().sum();
-    dbg!(result);
-}
-
-fn part2(filename: String) {
-    let (first, second) = extract(filename);
-    let terms: Vec<usize> = first
-        .iter()
-        .map(|x| *x as usize * second.iter().filter(|y| *y == x).count())
-        .collect();
-    // dbg!(&terms);
-    let result: usize = terms.iter().sum();
-    dbg!(result);
+    };
 }
