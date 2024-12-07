a47ce26cfb68d5ae2ab5377e8223c18986c407ee
Reinis Danne <rei4dan@gmail.com>
2024-12-07 18:33:53 +0200

    Add day 7 part 2

--- src/day07.rs
+++ src/day07.rs
@@ -3,7 +3,8 @@ use super::Args;
 pub(crate) fn run(args: Args) -> usize {
     let filename = args.filename();
     match args.part {
-        1 => part1(filename),
+        1 => part1(filename, 2),
+        2 => part1(filename, 3),
         _ => todo!(),
     }
 }
@@ -30,45 +31,45 @@ fn read(filename: String) -> Vec<(usize, Vec<usize>)> {
         .collect()
 }
 
-fn part1(filename: String) -> usize {
+fn part1(filename: String, op_count: usize) -> usize {
     let equations = read(filename);
     let result = equations
         .iter()
-        .filter(|&equation| possibly_valid(equation))
+        .filter(|&equation| possibly_valid(equation, op_count))
         .map(|(x, _)| x)
         .sum();
     dbg!(result);
     result
 }
 
-fn possibly_valid(equation: &(usize, Vec<usize>)) -> bool {
+fn possibly_valid(equation: &(usize, Vec<usize>), op_count: usize) -> bool {
     let (value, args) = equation;
-    solve(*value, args, 0)
+    solve(*value, args, 0, op_count)
 }
 
-fn solve(value: usize, args: &[usize], opi: usize) -> bool {
-    if opi == 2 {
+fn solve(value: usize, args: &[usize], opi: usize, op_count: usize) -> bool {
+    if opi == op_count {
         dbg_print(format!("Teriminating op {opi}: {value}: {args:?}"));
         return false;
     }
     if args.len() == 1 {
-        dbg_print(format!("Teriminating value op {opi}: {value}: {args:?}"));
+        dbg_print(format!(
+            "Teriminating value op {opi}: {value}: {args:?} {}",
+            if value == args[0] { "O" } else { "X" }
+        ));
         return value == args[0];
     }
-    let mut first = op(args[0], args[1], opi);
-    let args1 = &mut vec![first];
-    args1.extend(&args[2..]);
-    dbg_print(format!("Reducing {value}: {args:?} -> {args1:?}"));
-
-    if solve(value, args1, opi) {
-        true
-    } else {
-        first = op(args[0], args[1], opi + 1);
+    for i in 0..op_count {
+        let first = op(args[0], args[1], i);
         let args1 = &mut vec![first];
         args1.extend(&args[2..]);
-        dbg_print(format!("Reducing 2 {value}: {args:?} -> {args1:?}"));
-        solve(value, args1, opi)
+        dbg_print(format!("Reducing {value}: {args:?} -> {args1:?}"));
+
+        if solve(value, args1, i, op_count) {
+            return true;
+        }
     }
+    false
 }
 
 fn dbg_print(message: String) {
@@ -81,6 +82,7 @@ fn op(a: usize, b: usize, opi: usize) -> usize {
     match opi {
         0 => a + b,
         1 => a * b,
+        2 => format!("{a}{b}").parse().unwrap(),
         _ => todo!(),
     }
 }
@@ -90,4 +92,5 @@ mod tests {
     use crate::test;
 
     test!(p1, 7, 1, 1, 3749);
+    test!(p2, 7, 2, 1, 11387);
 }
