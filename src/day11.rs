a64a173b635848ce0aa4138079dc871e86d5bf80
Reinis Danne <rei4dan@gmail.com>
2024-12-11 22:30:10 +0200

    Add day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::mem;
+
 use crate::Args;
 use crate::DEBUG;
 use crate::ep;
@@ -7,6 +10,7 @@ pub(crate) fn run(args: Args) -> usize {
     let stones = &read(filename);
     match args.part {
         1 => part1(stones),
+        2 => part2(stones),
         _ => todo!(),
     }
 }
@@ -25,39 +29,50 @@ fn read(filename: String) -> Vec<usize> {
 }
 
 fn part1(stones: &[usize]) -> usize {
-    let stones = &mut stones.to_owned();
-    for step in 0..25 {
-        let len = stones.len();
-        let mut add = Vec::new();
-        for k in 0..len {
-            if stones[k] == 0 {
-                stones[k] = 1;
+    part(stones, 25)
+}
+
+fn part2(stones: &[usize]) -> usize {
+    part(stones, 75)
+}
+
+fn part(stones: &[usize], steps: usize) -> usize {
+    let mut stones: HashMap<usize, usize> = stones.iter().map(|&stone| (stone, 1)).collect();
+    let mut stones2: HashMap<usize, usize> = HashMap::new();
+    for step in 0..steps {
+        for (stone, count) in stones.iter() {
+            if *stone == 0 {
+                *stones2.entry(1).or_insert(0) += count;
                 continue;
             }
-            let stone = stones[k];
             let n = stone.ilog10() + 1;
             if n % 2 == 0 {
                 let factor = 10usize.pow(n / 2);
-                stones[k] /= factor;
-                add.push((k, stone % factor));
+                *stones2.entry(stone / factor).or_insert(0) += count;
+                *stones2.entry(stone % factor).or_insert(0) += count;
                 continue;
             }
-            stones[k] = stone * 2024;
+            *stones2.entry(stone * 2024).or_insert(0) += count;
         }
-        for (i, (k, stone)) in add.iter().enumerate() {
-            stones.insert(k + i + 1, *stone);
-        }
-        ep!("{step:02}: {}", stones.len());
-        // ep!("{stones:?}");
+        mem::swap(&mut stones, &mut stones2);
+        stones2.clear();
+        ep!("{step:02}: {}", stones.values().sum::<usize>());
+        // ep!("{stones:#?}");
     }
-    let count = stones.len();
+    let count = stones.values().sum();
     dbg!(count);
     count
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::bench;
     use crate::test;
 
     test!(p1, 11, 1, 1, 55312);
+
+    bench!(b1e, 11, 1, Some(1));
+    bench!(b1i, 11, 1, None);
+    bench!(b2e, 11, 2, Some(1));
+    bench!(b2i, 11, 2, None);
 }
